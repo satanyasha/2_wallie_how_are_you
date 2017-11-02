a529e7ef4181c356feaa0dc9f4ab6d4173f92cd3
satanyasha <33263995+satanyasha@users.noreply.github.com>
2017-11-02 20:56:25 +0500

    Update wallie.py

--- wallie.py
+++ wallie.py
@@ -18,4 +18,4 @@ def get_wallie_action():
 if __name__ == '__main__':
     action = get_wallie_action()
     print action
-git clone <https://github.com/satanyasha>
+git clone <https://github.com/satanyasha/2_wallie_how_are_you>
